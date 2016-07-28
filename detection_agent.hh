b46c36c8335a7514c667548d0161af9370b0b0e4
Jose Saldana <jsaldana@unizar.es>
2016-07-28 18:48:52 +0200

    Comments added

--- detection_agent.hh
+++ detection_agent.hh
@@ -19,7 +19,8 @@ DetectionAgent
 No ports
 
 =d
-Acts as an agent for the Odin controller. Its function is reporting about detected services
+Acts as an agent for the Odin controller. Its function is to report detected flows (services), 
+identified by 5-tuples (IP addresses, ports and protocol)
 
 =a
 Whatever
@@ -38,18 +39,18 @@ public:
   int configure(Vector<String> &, ErrorHandler *);
   void push(int, Packet *);
 
-
   
   // Classes and Methods to handle flows
+  // We use a 5-tuple to define a flow, including IP addresses, ports and protocol.
   class Flow {
     public:
         IPAddress src_ip;
         IPAddress dst_ip;
-		uint8_t protocol;
-		uint16_t src_port;
-		uint16_t dst_port;  
-		Timestamp last_flow_heard; // Stores the timestamp when a single flow has been heard
-		Timestamp last_flow_sent; // Stores the timestamp when the last FLOW message has been sent for a single flow
+	uint8_t protocol;
+	uint16_t src_port;
+	uint16_t dst_port;  
+	Timestamp last_flow_heard;	// Stores the timestamp when a single flow has been heard
+	Timestamp last_flow_sent;	// Stores the timestamp when the last FLOW message has been sent for a single flow
   };
 
   Vector<Flow> _flows_list;
@@ -57,7 +58,7 @@ public:
   //debug
   void print_flows_state();
 
-  int _debug_level;		//"0" no info displayed; "1" only basic info displayed; "2" all the info displayed; "1x" demo info displayed
+  int _debug_level;	//"0" no info displayed; "1" only basic info displayed; "2" all the info displayed; "1x" demo info displayed
 
 private:
   IPAddress _detection_agent_ip;
